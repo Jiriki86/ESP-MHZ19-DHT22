5617e7b3a18d29ef67c4dbf909470a559bcab240
Tobias Weber <toweber86@gmail.com>
2023-12-30 13:25:28 +0100

    working wifi implementation. Note that cfg.toml is not committed

---
+++ src/wifi.rs
@@ -0,0 +1,45 @@
+use anyhow::{bail, Result};
+use embedded_svc::wifi::{AuthMethod, ClientConfiguration, Configuration};
+use esp_idf_hal::peripheral;
+use esp_idf_svc::{eventloop::EspSystemEventLoop, wifi::BlockingWifi, wifi::EspWifi};
+use log::info;
+
+pub fn wifi(
+    ssid: &str,
+    pass: &str,
+    modem: impl peripheral::Peripheral<P = esp_idf_hal::modem::Modem> + 'static,
+    sysloop: EspSystemEventLoop,
+) -> Result<Box<EspWifi<'static>>> {
+    let auth_method = AuthMethod::WPA2Personal;
+    if ssid.is_empty() {
+        bail!("Missing WiFi name")
+    }
+    if pass.is_empty() {
+        bail!("Wifi password is empty");
+    }
+    let mut esp_wifi = EspWifi::new(modem, sysloop.clone(), None)?;
+    let mut wifi = BlockingWifi::wrap(&mut esp_wifi, sysloop)?;
+
+    let config = Configuration::Client(ClientConfiguration {
+        ssid: ssid.into(),
+        bssid: None,
+        auth_method,
+        password: pass.into(),
+        channel: None,
+    });
+    wifi.set_configuration(&config)?;
+
+    info!("Starting wifi...");
+    wifi.start()?;
+
+    info!("Connecting wifi...");
+    wifi.connect()?;
+
+    info!("Waiting for DHCP lease...");
+    wifi.wait_netif_up()?;
+
+    let ip_info = wifi.wifi().sta_netif().get_ip_info()?;
+    info!("Wifi DHCP info: {:?}", ip_info);
+
+    Ok(Box::new(esp_wifi))
+}
